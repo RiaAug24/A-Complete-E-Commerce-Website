e2086a316475417f53afd87d324d645d05f759c5
Riyaz Ahmed <riyaz04ahmed24aug.github@gmail.com>
2024-02-21 19:50:55 +0530

    Added toggle sidebar

--- JS/app.js
+++ JS/app.js
@@ -11,9 +11,21 @@ function copyMenu() {
   navPlace.innerHTML = mainNav.innerHTML;
 
   //copy .header-top .wrapper to .thetop-nav
-  const topNav = document.querySelector('.header-top');
-  const topPlace = document.querySelector('off-canvas .thetop-nav');
+  const topNav = document.querySelector('.header-top .wrapper');
+  const topPlace = document.querySelector('.off-canvas .thetop-nav');
   topPlace.innerHTML = topNav.innerHTML;
 
 }
 copyMenu();
+
+//show side-menubar on mobile
+
+const submenu = document.querySelectorAll(".has-child .icon-small");
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand');
+}
