c9b4c6cdf3d38a5f6dc692931f692176858e467f
Riyaz Ahmed <riyaz04ahmed24aug.github@gmail.com>
2024-02-21 20:15:41 +0530

    SideBar menu complete

--- JS/app.js
+++ JS/app.js
@@ -1,31 +1,42 @@
 //copy menu for mobile
 function copyMenu() {
   //copy insdie .dpt-cat to .departments
-  let dptCategory = document.querySelector('.dpt-cat');
-  let dptPlace = document.querySelector('.departments');
+  let dptCategory = document.querySelector(".dpt-cat");
+  let dptPlace = document.querySelector(".departments");
   dptPlace.innerHTML = dptCategory.innerHTML;
 
   //COPY NAV INSIDE NAV
-  const mainNav = document.querySelector('.header-nav nav'); 
-  const navPlace = document.querySelector('.off-canvas nav');
+  const mainNav = document.querySelector(".header-nav nav");
+  const navPlace = document.querySelector(".off-canvas nav");
   navPlace.innerHTML = mainNav.innerHTML;
 
   //copy .header-top .wrapper to .thetop-nav
-  const topNav = document.querySelector('.header-top .wrapper');
-  const topPlace = document.querySelector('.off-canvas .thetop-nav');
+  const topNav = document.querySelector(".header-top .wrapper");
+  const topPlace = document.querySelector(".off-canvas .thetop-nav");
   topPlace.innerHTML = topNav.innerHTML;
-
 }
 copyMenu();
 
+const menuButton = document.querySelector(".trigger");
+const closeButton = document.querySelector(".t-close");
+const addclass = document.querySelector(".site");
+
+menuButton.addEventListener('click', () => {
+  addclass.classList.toggle('showmenu')
+});
+closeButton.addEventListener('click', () => {
+  addclass.classList.remove('showmenu')
+});
 //show side-menubar on mobile
 
 const submenu = document.querySelectorAll(".has-child .icon-small");
-submenu.forEach((menu) => menu.addEventListener('click', toggle));
+submenu.forEach((menu) => menu.addEventListener("click", toggle));
 
 function toggle(e) {
-    e.preventDefault();
-    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
-    if (this.closest('.has-child').classList != 'expand');
-    this.closest('.has-child').classList.toggle('expand');
+  e.preventDefault();
+  submenu.forEach((item) =>
+    item != this ? item.closest(".has-child").classList.remove("expand") : null
+  );
+  if (this.closest(".has-child").classList != "expand");
+  this.closest(".has-child").classList.toggle("expand");
 }
